c69b735744ea9fc0350419ba570e98877a0ca1ae
XLor <yjl9903@vip.qq.com>
2022-04-23 01:54:53 +0800

    docs: optional arg

--- examples/echo.ts
+++ examples/echo.ts
@@ -7,3 +7,7 @@ export const version = '0.0.0';
 export default function(text: string) {
   console.log(text);
 }
+
+export function greet(name?: string) {
+  console.log(`Hello, ${ name ?? 'Stranger' }`);
+}
