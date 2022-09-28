a9929883c2bc01e81bfb4c9b2236bfb879b6b8cb
XLor <yjl9903@vip.qq.com>
2022-09-28 10:14:13 +0000

    feat: global dts

--- src/space.ts
+++ src/space.ts
@@ -1,6 +1,7 @@
 import os from 'node:os';
 import path from 'node:path';
 import fs from 'fs-extra';
+import { fileURLToPath } from 'node:url';
 
 export const OPTC_ROOT = process.env.OPTC_ROOT
   ? path.resolve(process.env.OPTC_ROOT)
@@ -10,6 +11,7 @@ export async function ensureSpace() {
   await fs.ensureDir(OPTC_ROOT);
   const dep = path.join(OPTC_ROOT, 'dep.ts');
   const pkg = path.join(OPTC_ROOT, 'package.json');
+  const dts = path.join(OPTC_ROOT, 'globals.d.ts');
   if (!fs.existsSync(pkg)) {
     await fs.writeFile(
       pkg,
@@ -28,5 +30,10 @@ export async function ensureSpace() {
   if (!fs.existsSync(dep)) {
     const body = `export default function(global: any) {\n\n}`;
     await fs.writeFile(dep, body, 'utf-8');
+  }
+  if (!fs.existsSync(dts)) {
+    const globalsDts = path.join(fileURLToPath(import.meta.url), '../../globals.d.ts');
+    const body = `/// <reference path="${globalsDts}" />`;
+    await fs.writeFile(dts, body, 'utf-8');
   }
 }
