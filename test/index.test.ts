4e048b4663f4a2eeab08f0fd36cb88bae2e5e894
XLor <yjl9903@vip.qq.com>
2022-10-20 18:20:33 +0000

    test: more testcase

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,6 +2,20 @@ import { describe, expect, it } from 'vitest';
 
 import { bootstrap } from '../src';
 
+describe('Examples', () => {
+  it('http', () => {
+    expect(() => bootstrap('examples/http.ts', 'http://numbersapi.com/42')).not.toThrow();
+  });
+
+  it('node', () => {
+    expect(() => bootstrap('examples/node.ts')).not.toThrow();
+  });
+
+  it('pkg', () => {
+    expect(() => bootstrap('examples/pkg.ts', '--root', '.')).not.toThrow();
+  });
+});
+
 describe('Preset', () => {
   it('should work', async () => {
     expect(await bootstrap('test/fixtures/msg.ts')).toMatchInlineSnapshot('"Hello, world."');
---
+++ test/refl.test.ts
@@ -0,0 +1,47 @@
+import { describe, expect, it } from 'vitest';
+
+import { makeOptc } from '../src';
+
+describe('Preset', () => {
+  it('should work', async () => {
+    const cli = await makeOptc('test/fixtures/option.ts');
+    expect(cli.getRawCommands()).toMatchInlineSnapshot(`
+      [
+        {
+          "default": true,
+          "description": "This is a default command",
+          "name": "",
+          "options": [
+            {
+              "description": "This is root",
+              "name": "root",
+              "required": true,
+              "type": "string",
+            },
+          ],
+          "parameters": [],
+        },
+        {
+          "default": false,
+          "description": "Say hello",
+          "name": "hello",
+          "options": [
+            {
+              "description": "",
+              "name": "recv",
+              "required": true,
+              "type": "string",
+            },
+          ],
+          "parameters": [
+            {
+              "name": "msg",
+              "required": true,
+              "type": "string[]",
+            },
+          ],
+        },
+      ]
+    `);
+  });
+});
